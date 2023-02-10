b0982f34cd48a9b3781c0cfc2ded43bcf07ffb02
Anatoliy Agadzhanov <aanatoliy17@gmail.com>
2023-02-10 15:25:33 -0500

    edited a space

--- server.js
+++ server.js
@@ -2,10 +2,13 @@ const express = require('express')
 const routes = require('./routes')
 const db = require('./db')
 const logger = require('morgan')
+const cors = require('cors')
 
 const PORT = process.env.PORT || 3001
 
 const app = express()
+
+app.use(cors())
 
 app.use(express.json())
 
