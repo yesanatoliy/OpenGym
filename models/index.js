66b86531aea41b353c284dbc6e45d7d56fe48339
Anatoliy Agadzhanov <aanatoliy17@gmail.com>
2023-02-10 14:00:20 -0500

    finished models

--- models/index.js
+++ models/index.js
@@ -3,14 +3,12 @@ const userSchema = require('./user')
 const eventSchema = require('./event')
 const commentSchema = require('./comment')
 
-
-
-
-
-
-
-
+const User = mongoose.model('User', userSchema)
+const Comment = mongoose.model('Comment', commentSchema)
+const Event = mongoose.model('Event', eventSchema)
 
 module.exports = {
-
+    User,
+    Event,
+    Comment
 }
