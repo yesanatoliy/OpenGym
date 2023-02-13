11249d62b3b00b18ecfef95cc2c0d67f731eb337
Anatoliy Agadzhanov <aanatoliy17@gmail.com>
2023-02-13 14:32:27 -0500

    made user and comment controllers, holding off get all comments controller until i have a better way to test it

--- controllers/index.js
+++ controllers/index.js
@@ -47,7 +47,7 @@ const getEventById = async (req,res) => {
     try{
         const { id } = req.params
         const event = await Event.findById(id)
-        if (plant) {
+        if (event) {
             return res.status(200).json({ event })
         }
         return res.status(404).send(error.message)
@@ -55,6 +55,13 @@ const getEventById = async (req,res) => {
         return res.status(500).send(error.message)
     }
 }
+
+// const getComments = async (req,res) => {
+//     try{
+//         const { id } = req.params
+
+//     }
+// }
 
 const createComment = async (req, res) => {
     try{
@@ -68,6 +75,66 @@ const createComment = async (req, res) => {
     }
 }
 
+const updateComment = async (req, res) => {
+    try{
+        const comment = await Comment.findByIdAndUpdate(req.params.id, req.body, {new: true})
+        res.status(200).json(comment)
+    } catch (error) {
+        return res.status(500).send(error.message)
+    }
+}
+
+const deleteComment = async (req,res) => {
+    try{
+        const { id } = req.params
+        const deleted = await Comment.findByIdAndDelete(id)
+        if (deleted) {
+            return res.status(200).send("Comment deleted")
+        }
+        throw new Error("Comment not found")
+    } catch (error) {
+        return res.status(500).send(error.message)
+    }
+}
+
+const getUser = async (req,res) => {
+    try{
+        const { id } = req.params
+        const user = await User.findById(id)
+        if (user) {
+            return res.status(200).json({ user })
+        }
+        return res.status(404).send(error.message)
+    } catch (error) {
+        return res.status(500).send(error.message)
+    }
+}
+
+const createUser = async (req, res) => {
+    try{
+        const user = await new User(req.body)
+        await user.save()
+        return res.status(201).json({
+            user
+        })
+    } catch (error) {
+        return res.status(500).json({ error: error.message })
+    }
+}
+
+const deleteUser = async (req,res) => {
+    try{
+        const { id } = req.params
+        const deleted = await User.findByIdAndDelete(id)
+        if (deleted) {
+            return res.status(200).send("User deleted")
+        }
+        throw new Error("User not found")
+    } catch (error) {
+        return res.status(500).send(error.message)
+    }
+}
+
 module.exports = {
     getAllEvents,
     createEvent,
@@ -75,4 +142,9 @@ module.exports = {
     deleteEvent,
     getEventById,
     createComment,
+    updateComment,
+    deleteComment,
+    getUser,
+    createUser,
+    deleteUser
 }
