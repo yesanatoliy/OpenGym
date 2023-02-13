e333682e763f47b1b7f4e3c08b8317f5bfced09f
Anatoliy Agadzhanov <aanatoliy17@gmail.com>
2023-02-13 17:06:30 -0500

    fixed seed by using individual variables for each event

--- seed/userEventComment.js
+++ seed/userEventComment.js
@@ -12,50 +12,77 @@ const main = async () => {
         name: 'Jordan1'
     })
     user2.save()
-    const events = [
-        {
-            user_id: user1._id,
-            name: 'a5',
-            address: 'roswell rd',
-            cost: 5,
-            level: 'C-Open',
-            contact: '123@123.com',
-            date: '01/10/23',
-            time: '6:15pm',
-            description: 'good gym'
-        },
-        {
-            user_id: user2._id,
-            name: 'tk',
-            address: 'roswell rd',
-            cost: 7,
-            level: 'B-Open',
-            contact: '123@123.com',
-            date: '01/10/23',
-            time: '7pm',
-            description: 'good gym'
-        }
-    ]
-    await Event.insertMany(events)
+    // const events = [
+    //     {
+    //         user_id: user1._id,
+    //         name: 'a5',
+    //         address: 'roswell rd',
+    //         cost: 5,
+    //         level: 'C-Open',
+    //         contact: '123@123.com',
+    //         date: '01/10/23',
+    //         time: '6:15pm',
+    //         description: 'good gym'
+    //     },
+    //     {
+    //         user_id: user2._id,
+    //         name: 'tk',
+    //         address: 'roswell rd',
+    //         cost: 7,
+    //         level: 'B-Open',
+    //         contact: '123@123.com',
+    //         date: '01/10/23',
+    //         time: '7pm',
+    //         description: 'good gym'
+    //     }
+    // ]
+    // await Event.insertMany(events)
+    const event1 = await new Event({
+        user_id: user1._id,
+        name: 'a5',
+        address: 'roswell rd',
+        cost: 5,
+        level: 'C-Open',
+        contact: '123@123.com',
+        date: '01/10/23',
+        time: '6:15pm',
+        description: 'good gym'
+    })
+    event1.save()
+
+    const event2 = await new Event({
+        user_id: user1._id,
+        name: 'a5',
+        address: 'roswell rd',
+        cost: 5,
+        level: 'C-Open',
+        contact: '123@123.com',
+        date: '01/10/23',
+        time: '6:15pm',
+        description: 'bad gym'
+    })
+    event2.save()
+    
+    console.log('events added')
     const comments = [
         {
             user_id: user1._id,
-            event_id: events[0]._id,
+            event_id: event1._id,
             contents: `you get to stay on all night if you win!`
         },
         {
             user_id: user1._id,
-            event_id: events[1]._id,
+            event_id: event2._id,
             contents: `you get to stay on all night if you win or lose!`
         },
         {
             user_id: user2._id,
-            event_id: events[0]._id,
+            event_id: event1._id,
             contents: `this place sucks!`
         },
         {
             user_id: user2._id,
-            event_id: events[1]._id,
+            event_id: event2._id,
             contents: `this place rules!`
         }
     ]
