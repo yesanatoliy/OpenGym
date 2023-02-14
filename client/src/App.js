38ca12d85e2ddcb1b059e53a94e98617ace6145e
Anatoliy Agadzhanov <aanatoliy17@gmail.com>
2023-02-14 10:57:54 -0500

    started navbar and home components

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
-import './App.css';
-
+import './App.css'
+import Home from './components/Home'
+import NavBar from './components/NavBar'
 
 function App() {
   return (
     <div className="App">
+      <header>
+        <NavBar />
+      </header>
       <Home />
     </div>
   );
