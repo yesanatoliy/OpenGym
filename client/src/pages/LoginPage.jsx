a5efd2242eab28be18d934dab2dcba0abd89dbbb
Anatoliy Agadzhanov <aanatoliy17@gmail.com>
2023-02-15 12:02:40 -0500

    made the sign up page form on the login page

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,15 +1,39 @@
 import { useState } from 'react'
 import axios from 'axios'
+import { useNavigate } from "react-router-dom"
 
 const LoginPage = () => {
+  const navigate = useNavigate()
+
   const initialState = {
     email: '',
     password: ''
-    }
+  }
+
+  const [formState, setFormState] = useState(initialState)
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    await axios.post('/api/user', formState)
+    setFormState(initialState)
+    navigate(`/home`)
+  }
+
+  const handleChange = (e) => {
+    setFormState(
+      {...formState, [e.target.id]: e.target.value}
+    )
+  }
   
   
   return (
-    <div>LoginPage</div>
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="email">Email</label>
+      <input type="text" id="email" onChange={handleChange} value={formState.email} />
+      <label htmlFor="password">Password</label>
+      <input type="text" id="password" onChange={handleChange} value={formState.password} />
+      <button type="submit">Sign Up</button>
+    </form>
   )
 }
 
