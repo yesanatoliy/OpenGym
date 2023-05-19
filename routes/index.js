92d4f49f5f9ab97a6f43818e6607635afcd38478
Anatoliy Agadzhanov <aanatoliy17@gmail.com>
2023-05-19 16:14:00 -0400

    protected some routes in routes.js using the middleware, now i'm realizing i'll have to make sure the controllers properly check to make sure you can edit only your own events, comments and profiles though

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,20 @@ const router = Router()
 const middleware = require('../middleware')
 
 router.get('/', (req, res) => res.send('This is root!'))
-router.post('/events/user/:userId', controllers.createEvent)
+router.post('/events/user/:userId', 
+middleware.stripToken,
+middleware.verifyToken,
+controllers.createEvent)
 router.get('/events', controllers.getAllEvents)
 router.get('/events/:eventId', controllers.getEventById)
-router.put('/events/:eventId', controllers.updateEvent)
-router.delete('/events/:eventId', controllers.deleteEvent)
+router.put('/events/:eventId', 
+middleware.stripToken,
+middleware.verifyToken,
+controllers.updateEvent)
+router.delete('/events/:eventId', 
+middleware.stripToken,
+middleware.verifyToken,
+controllers.deleteEvent)
 router.post('/user', controllers.createUser)
 router.post('/login', controllers.signIn)
 router.get('/session', 
@@ -17,11 +26,26 @@ middleware.verifyToken,
 controllers.checkSession)
 router.get('/profile/:userId', controllers.getUser)
 router.get('/user/:username', controllers.getUserByUsername)
-router.put('/user/:userId', controllers.updateUser)
-router.delete('/user/:userId', controllers.deleteUser)
+router.put('/user/:userId', 
+middleware.stripToken,
+middleware.verifyToken,
+controllers.updateUser)
+router.delete('/user/:userId', 
+middleware.stripToken,
+middleware.verifyToken,
+controllers.deleteUser)
 router.get('/events/:eventId/comments', controllers.getEventComments)
-router.put('/comments/:id', controllers.updateComment)
-router.delete('/comments/:id', controllers.deleteComment)
-router.post('/events/:eventId/comments/:userId', controllers.createComment)
+router.put('/comments/:id', 
+middleware.stripToken,
+middleware.verifyToken,
+controllers.updateComment)
+router.delete('/comments/:id', 
+middleware.stripToken,
+middleware.verifyToken,
+controllers.deleteComment)
+router.post('/events/:eventId/comments/:userId',
+middleware.stripToken,
+middleware.verifyToken,
+controllers.createComment)
 
 module.exports = router
